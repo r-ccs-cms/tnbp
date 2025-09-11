b8b2c987134932f9eae26a91bbac46c1255c390d
t-sirakawa <t-shirakawa@riken.jp>
2025-09-12 08:31:58 +0900

    add tci::traits

---
+++ external/minitci/include/tci/traits.h
@@ -0,0 +1,141 @@
+/// This file is a part of r-ccs-cms/tnbp
+/**
+@file tci/traits.h
+@brief header to define traits
+ */
+#ifndef TCI_TRAITS_H
+#define TCI_TRAITS_H
+namespace tci {
+  template <typename TenT>
+  struct tensor_traits;
+
+  // traits for gqten::tensor<float>
+  template <>
+  struct tensor_traits<gqten::tensor<float>> {
+    using ten_t = gqten::tensor<float>;
+    using rank_t = int32_t;
+    using shape_t = std::vector<size_t>;
+    using bond_dim_t = size_t;
+    using bond_idx_t = size_t;
+    using bond_label_t = int32_t;
+    using ten_size_t = size_t;
+    using elem_t = float;
+    using elem_coor_t = size_t;
+    using elem_coors_t = std::vector<size_t>;
+    using real_t = float;
+    using real_ten_t = gqten::tensor<float>;
+    using cplx_t = std::complex<float>;
+    using cplx_ten_t = gqten::tensor<std::complex<float>>;
+    using context_handle_t = std::string;
+  };
+
+  
+  // traits for gqten::tensor<double>
+  template <>
+  struct tensor_traits<gqten::tensor<double>> {
+    using ten_t = gqten::tensor<double>;
+    using rank_t = int32_t;
+    using shape_t = std::vector<size_t>;
+    using bond_dim_t = size_t;
+    using bond_idx_t = size_t;
+    using bond_label_t = int32_t;
+    using ten_size_t = size_t;
+    using elem_t = double;
+    using elem_coor_t = size_t;
+    using elem_coors_t = std::vector<size_t>;
+    using real_t = double;
+    using real_ten_t = gqten::tensor<double>;
+    using cplx_t = std::complex<double>;
+    using cplx_ten_t = gqten::tensor<std::complex<double>>;
+    using context_handle_t = std::string;
+  };
+  
+  // traits for gqten::tensor<std::complex<float>>
+  template <>
+  struct tensor_traits<gqten::tensor<std::complex<float>>> {
+    using ten_t = gqten::tensor<std::complex<float>>;
+    using rank_t = int32_t;
+    using shape_t = std::vector<size_t>;
+    using bond_dim_t = size_t;
+    using bond_idx_t = size_t;
+    using bond_label_t = int32_t;
+    using ten_size_t = size_t;
+    using elem_t = std::complex<float>;
+    using elem_coor_t = size_t;
+    using elem_coors_t = std::vector<size_t>;
+    using real_t = float;
+    using real_ten_t = gqten::tensor<float>;
+    using cplx_t = std::complex<float>;
+    using cplx_ten_t = gqten::tensor<std::complex<float>>;
+    using context_handle_t = std::string;
+  };
+
+  // traits for gqten::tensor<std::complex<double>>
+  template <>
+  struct tensor_traits<gqten::tensor<std::complex<double>>> {
+    using ten_t = gqten::tensor<std::complex<double>>;
+    using rank_t = int32_t;
+    using shape_t = std::vector<size_t>;
+    using bond_dim_t = size_t;
+    using bond_idx_t = size_t;
+    using bond_label_t = int32_t;
+    using ten_size_t = size_t;
+    using elem_t = std::complex<double>;
+    using elem_coor_t = size_t;
+    using elem_coors_t = std::vector<size_t>;
+    using real_t = double;
+    using real_ten_t = gqten::tensor<double>;
+    using cplx_t = std::complex<double>;
+    using cplx_ten_t = gqten::tensor<std::complex<double>>;
+    using context_handle_t = std::string;
+  };
+  
+  template <typename TenT>
+  using ten_t = typename tensor_traits<TenT>::ten_t;
+
+  template <typename TenT>
+  using rank_t = typename tensor_traits<TenT>::rank_t;
+
+  template <typename TenT>
+  using shape_t = typename tensor_traits<TenT>::shape_t;
+
+  template <typename TenT>
+  using bond_dim_t = typename tensor_traits<TenT>::bond_dim_t;
+
+  template <typename TenT>
+  using bond_idx_t = typename tensor_traits<TenT>::bond_idx_t;
+
+  template <typename TenT>
+  using bond_label_t = typename tensor_traits<TenT>::bond_label_t;
+
+  template <typename TenT>
+  using ten_size_t = typename tensor_traits<TenT>::ten_size_t;
+
+  template <typename TenT>
+  using elem_t = typename tensor_traits<TenT>::elem_t;
+
+  template <typename TenT>
+  using elem_coor_t = typename tensor_traits<TenT>::elem_coor_t;
+
+  template <typename TenT>
+  using elem_coors_t = typename tensor_traits<TenT>::elem_coors_t;
+
+  template <typename TenT>
+  using real_t = typename tensor_traits<TenT>::real_t;
+
+  template <typename TenT>
+  using real_ten_t = typename tensor_traits<TenT>::real_ten_t;
+
+  template <typename TenT>
+  using cplx_t = typename tensor_traits<TenT>::cplx_t;
+
+  template <typename TenT>
+  using cplx_ten_t = typename tensor_traits<TenT>::cplx_ten_t;
+
+  template <typename TenT>
+  using context_handle_t = typename tensor_traits<TenT>::context_handle_t;
+    
+}
+#endif
+
+
